c370108d98ee54bba0320f1d2421228d8809d13d
Arseniy Usik <senya769@mail.ru>
2022-03-15 21:05:47 +0300

    Initial commit

---
+++ src/arseniyusik/lesson3/TaskOne.java
@@ -0,0 +1,12 @@
+package arseniyusik.lesson3;
+
+public class TaskOne {
+    public static void main(String[] args) {
+
+        int Number = 123;
+        while(Number != 0 )
+        {
+            
+        }
+    }
+}
