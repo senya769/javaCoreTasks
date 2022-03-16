b35f781f008c316ebc8d99ed896d2788ff5cb31e
Arseniy Usik <senya769@mail.ru>
2022-03-16 16:26:44 +0300

    Uptade the Task4

--- src/arseniyusik/lesson3/Task4.java
+++ src/arseniyusik/lesson3/Task4.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
         int valueOne = scan.nextInt();
         int valueTwo = scan.nextInt();
         int valueThree = scan.nextInt();
@@ -12,6 +13,6 @@ public class Task4 {
         if (valueOne > 0) temp++;
         if (valueTwo > 0) temp++;
         if (valueThree > 0) temp++;
-        System.out.println("from a set of numbers" + temp + " positive number");
+        System.out.println("from a set of numbers " + temp + " positive number");
     }
 }
