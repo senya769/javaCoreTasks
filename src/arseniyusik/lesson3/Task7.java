c446f4935a895bebc8ce5867ab65125f0d1f4015
Arseniy Usik <senya769@mail.ru>
2022-03-16 17:07:19 +0300

    Uptade the Task7

--- src/arseniyusik/lesson3/Task7.java
+++ src/arseniyusik/lesson3/Task7.java
@@ -1,7 +1,16 @@
 package arseniyusik.lesson3;
 
+import java.util.Scanner;
+
 public class Task7 {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите количество людей:" );
+        int amountOfHuman = scan.nextInt();
+        String profession = "программист";
+        if (amountOfHuman > 0 && amountOfHuman < 5) profession += "а";
+        else if (amountOfHuman >= 5 || amountOfHuman == 0) profession += "ов";
+        else  if (amountOfHuman < 0) {System.out.println("Ошибка! Введите положительное число!"); return;}
+        System.out.println(amountOfHuman + " " + profession);
     }
 }
