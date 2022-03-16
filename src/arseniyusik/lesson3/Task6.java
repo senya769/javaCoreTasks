e68443a9e8bb6e0f998ff6aafde6714466e7cc3e
Arseniy Usik <senya769@mail.ru>
2022-03-16 16:37:04 +0300

    Uptade the Task6

--- src/arseniyusik/lesson3/Task6.java
+++ src/arseniyusik/lesson3/Task6.java
@@ -1,7 +1,16 @@
 package arseniyusik.lesson3;
 
+import java.util.Scanner;
+
 public class Task6 {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int valueOne = scan.nextInt();
+        System.out.println("Enter the second number: ");
+        int valueTwo = scan.nextInt();
+        if(valueOne > valueTwo) System.out.println("higher number: "+ valueOne);
+        else if (valueOne < valueTwo) System.out.println("higher number: "+ valueOne);
+        else System.out.println("the numbers are equal");
     }
 }
