c9072416a6ebacfe8c085293b609ebbefff7e4e2
Arseniy Usik <senya769@mail.ru>
2022-03-16 15:54:54 +0300

    add all files of tasks

---
+++ src/arseniyusik/lesson3/Task5.java
@@ -0,0 +1,7 @@
+package arseniyusik.lesson3;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/arseniyusik/lesson3/Task6.java
@@ -0,0 +1,7 @@
+package arseniyusik.lesson3;
+
+public class Task6 {
+    public static void main(String[] args) {
+
+    }
+}
