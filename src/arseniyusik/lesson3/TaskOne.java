02761e54f88d70076494fa72632a6a224e1159ff
Arseniy Usik <senya769@mail.ru>
2022-03-16 15:30:54 +0300

    Update tusk1

--- src/arseniyusik/lesson3/TaskOne.java
+++ src/arseniyusik/lesson3/TaskOne.java
@@ -1,42 +1,36 @@
 package arseniyusik.lesson3;
 
-import com.sun.jdi.Value;
-
 public class TaskOne {
     public static void main(String[] args) {
         String ValueType;
         int number = 1233;
-        if (number >= 0)
-        {
-            ValueType =  "Это число "+number + " положительное";
-        }
-        else {
-            ValueType ="Это число " + number + " отрицательное";
+        if (number >= 0) {
+            ValueType = "Это число " + number + " положительное";
+        } else {
+            ValueType = "Это число " + number + " отрицательное";
         }
         int count = 0;
-        for(int i = number; i!= 0;)
-        {
-        i /= 10;
-        count++;
+        for (int i = number; i != 0; ) {
+            i /= 10;
+            count++;
         }
-        switch(count)
-        {
-            case 1:{
+        switch (count) {
+            case 1: {
                 ValueType += " и оно однозначное.";
                 break;
             }
-            case 2:{
+            case 2: {
                 ValueType += " и оно двухзначное.";
                 break;
             }
-            case 3:{
+            case 3: {
                 ValueType += " и оно трехзначное.";
-                break; }
+                break;
+            }
 
             default:
                 ValueType += " и оно многозначное.";
         }
         System.out.println(ValueType);
-
     }
 }
