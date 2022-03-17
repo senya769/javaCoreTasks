5fc82b8e356c7131b77b8a4358933f2378d119f4
Arseniy Usik <senya769@mail.ru>
2022-03-17 09:01:49 +0300

    bug fixes 2.0 in Task7

--- src/arseniyusik/lesson3/Task7.java
+++ src/arseniyusik/lesson3/Task7.java
@@ -7,10 +7,9 @@ public class Task7 {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите количество людей:" );
         int amountOfHuman = scan.nextInt();
-        int amountEnd = amountOfHuman % 10;
         String profession = "программист";
-        if (amountEnd> 0 && amountEnd  < 5) profession += "а";
-        else if (amountEnd >= 5 || amountEnd == 0) profession += "ов";
+        if (amountOfHuman> 0 && amountOfHuman  < 5) profession += "а";
+        else if (amountOfHuman >= 5 || amountOfHuman == 0) profession += "ов";
         else  if (amountOfHuman < 0) {System.out.println("Ошибка! Введите положительное число!"); return;}
         System.out.println(amountOfHuman + " " + profession);
     }
