16f92e1509e4feb68216ba4dabc05275e2a189c8
Arseniy Usik <senya769@mail.ru>
2022-03-17 12:53:57 +0300

    fixed bugs for 11 - 20

--- src/arseniyusik/lesson3/Task7.java
+++ src/arseniyusik/lesson3/Task7.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class Task7 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Введите количество людей:" );
+        System.out.print("Введите количество людей:");
         int amountOfHuman = scan.nextInt();
+        int amountEnd10 = amountOfHuman % 10;
+        int amountEnd100 = amountOfHuman % 100;
         String profession = "программист";
-        if (amountOfHuman> 0 && amountOfHuman  < 5) profession += "а";
-        else if (amountOfHuman >= 5 || amountOfHuman == 0) profession += "ов";
-        else  if (amountOfHuman < 0) {System.out.println("Ошибка! Введите положительное число!"); return;}
+        if (amountEnd100 >= 5 && amountEnd100 <= 20) profession += "ов";
+        else if (amountEnd10 > 1 && amountEnd10 < 5) profession += "а";
+        else if (amountEnd10 == 1) { profession +="";
+        } else profession += "ов";
         System.out.println(amountOfHuman + " " + profession);
-        System.out.println("new");
     }
 }
