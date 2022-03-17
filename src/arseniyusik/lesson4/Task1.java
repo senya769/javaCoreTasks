b5db32662f1e210f863a6db1d17953e17b0ac57c
Arseniy Usik <senya769@mail.ru>
2022-03-17 12:58:41 +0300

    Create the first tusk1

---
+++ src/arseniyusik/lesson4/Task1.java
@@ -0,0 +1,9 @@
+package arseniyusik.lesson4;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
