09cdc8438a154e186dddef7c7f62fea7908c9f11
Arseniy Usik <senya769@mail.ru>
2022-03-18 21:54:32 +0300

    finish the Task6

---
+++ src/arseniyusik/lesson4/Task6.java
@@ -0,0 +1,10 @@
+package arseniyusik.lesson4;
+
+public class Task6 {
+    public static void main(String[] args) {
+        System.out.println("Вывод четных чисел в диапозоне [2;100]");
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
