15b4275d3c3f1eafdfd3c19765d6f44f71550828
Arseniy Usik <senya769@mail.ru>
2022-03-18 14:58:34 +0300

    finish the Task2

--- src/arseniyusik/lesson4/Tusk2.java
+++ src/arseniyusik/lesson4/Tusk2.java
@@ -2,6 +2,11 @@ package arseniyusik.lesson4;
 
 public class Tusk2 {
     public static void main(String[] args) {
-
+        int amountAmoeba = 1;
+        int hours = 3;
+        for (int i = hours; i < 24; i += 3) {
+            amountAmoeba *= 2;
+            System.out.println("Количество амеб за " + i + " часов равно " + amountAmoeba);
+        }
     }
 }
