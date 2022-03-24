41f2b400ff964338351804ecf6c69c3cf74090d1
Arseniy Usik <senya769@mail.ru>
2022-03-24 15:11:28 +0300

    SeaBattle upgrade to new branch

--- src/arseniyusik/lesson6/SeaBattle.java
+++ src/arseniyusik/lesson6/SeaBattle.java
@@ -19,54 +19,16 @@ public class SeaBattle {
                 place[i][j] = 'o';
             }
         }
-
+int sizeSheep =4;
         int randomShipPosition = rand.nextInt(2);
         int temp = 0;
         switch (randomShipPosition) {
             case 0:  // горизонтальное положение
-                while (temp < 4) {
-                    if (col_id > 6) {
-                        for (int j = col_id; j < 10; j++) {
-                            place[row_id][j] = 'X';
-                            temp++;
-                        }
-                    } else {
-                        for (int j = col_id; j < col_id + 4; j++) {
-                            place[row_id][j] = 'X';
-                            temp++;
-                        }
-                    }
-                    if (temp < 4) {
-                        for (int i = 0; i < 4 - temp; i++) {
-                            place[row_id][col_id - (4 - temp) + i] = 'X';
-                        }
-                        temp++;
-                    } else {
-                        break;
-                    }
-                }
+                insertPositionHorizotal(place,col_id,row_id,sizeSheep);
                 break;
             case 1:
-                while (temp < 4) {
-                    if (row_id > 6) {
+                insertPositionVertical(place,row_id,col_id,sizeSheep);
 
-                        for (int j = row_id; j < 10; j++) {
-                            place[j][col_id] = 'X';
-                            temp++;
-                        }
-                    } else {
-                        for (int j = row_id; j < row_id + 4; j++) {
-                            place[j][col_id] = 'X';
-                            temp++;
-                        }
-                    }
-                    if (temp < 4) {
-                        for (int i = 0; i < 4 - temp; i++) {
-                            place[row_id - (4 - temp) + i][col_id] = 'X';
-                        }
-                        temp++;
-                    }
-                }
                 break;
         }
 
@@ -78,34 +40,56 @@ public class SeaBattle {
             System.out.println();
         }
     }
+
+
+   public static void insertPositionHorizotal(char[][]place, int col_id, int row_id,int sizeShip){
+           int temp = 0;
+
+           while (temp < sizeShip) {
+               if (col_id > 10 - sizeShip) {
+
+                   for (int j = col_id; j < 10; j++) {
+                       place[row_id][j] = 'X';
+                       temp++;
+                   }
+               } else {
+                   for (int j = col_id; j < col_id + sizeShip; j++) {
+                       place[row_id][j] = 'X';
+                       temp++;
+                   }
+               }
+               if (temp < sizeShip) {
+                   for (int i = 0; i < sizeShip - temp; i++) {
+                       place[row_id][col_id - (sizeShip - temp) + i] = 'X';
+                   }
+                   temp++;
+               }
+           }
 }
+public static void insertPositionVertical(char[][]place, int col_id, int row_id,int sizeShip){
+           int temp = 0;
 
-/*
-   public void insertHorizontalPosition(char[][]place, int col_id, int row_id){
-        int temp = 0;
+           while (temp < sizeShip) {
+               if (row_id > 10 - sizeShip) {
 
-        while (col_id < 10) {
-            if (col_id > 6) {
-
-                for (int j = col_id; j < 10; j++) {
-                    place[row_id][j] = 'X';
-                    temp++;
-                }
-            } else {
-                for (int j = col_id; j < col_id + 4; j++) {
-                    place[row_id][j] = 'X';
-                    temp++;
-                }
-            }
-            if (temp < 4) {
-                for (int i = 0; i < 4 - temp; i++) {
-                    place[row_id][col_id - (4 - temp) + i] = 'X';
-                }
-                temp++;
-            } else {
-                break;
-            }
-        }
+                   for (int j = row_id; j < 10; j++) {
+                       place[j][col_id] = 'X';
+                       temp++;
+                   }
+               } else {
+                   for (int j = row_id; j < row_id + sizeShip; j++) {
+                       place[j][col_id] = 'X';
+                       temp++;
+                   }
+               }
+               if (temp < sizeShip) {
+                   for (int i = 0; i < sizeShip - temp; i++) {
+                       place[row_id - (sizeShip - temp) + i][col_id] = 'X';
+                   }
+                   temp++;
+               }
+           }
 }}
 
-* */
+
+
