9a184a8d800212ef591b4b2f87b8ab8d5a8afaab
Arseniy Usik <senya769@mail.ru>
2022-04-13 22:57:25 +0300

    achieve the 1 task from EnumTasks

---
+++ src/arseniyusik/lessonOOP/tasksEnum/Application.java
@@ -0,0 +1,17 @@
+package arseniyusik.lessonOOP.tasksEnum;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Season likeSeason = Season.SUMMER;
+        likeSeason.getFavoriteSeason();
+        System.out.println();
+        for (Season j: Season.values()) {
+            System.out.println(j + " temp:(" +j.getAverageTemperature()+") ");
+            j.getDescription();
+            System.out.println("----------------------------");
+        }
+    }
+    }
+
+
---
+++ src/arseniyusik/lessonOOP/tasksEnum/Season.java
@@ -0,0 +1,45 @@
+package arseniyusik.lessonOOP.tasksEnum;
+
+public enum Season {
+    WINTER(-5.2),
+    SPRING(9.2),
+    SUMMER(17.2){
+        @Override
+        public void getDescription() {
+            System.out.println("Теплая пора года");
+        }
+    },
+    AUTUMN(8.9);
+    private final double averageTemperature;
+
+    Season(double averageTemperature) {
+        this.averageTemperature = averageTemperature;
+    }
+
+    public double getAverageTemperature() {
+        return averageTemperature;
+    }
+
+    public void getFavoriteSeason() {
+        switch (this) {
+            case AUTUMN:
+                System.out.println("Я люблю осень!");
+                break;
+            case SPRING:
+                System.out.println("Я люблю весню!");
+                break;
+            case SUMMER:
+                System.out.println("Я люблю лето!");
+                break;
+            case WINTER:
+                System.out.println("Я люблю зиму!");
+                break;
+            default:
+                System.out.println("Я не люблю никакую пору года!");
+        }
+    }
+
+    public void getDescription() {
+        System.out.println("Холодная пора года");
+    }
+}
