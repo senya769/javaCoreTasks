b3bdaae9d3f483103ad078e90157af3c2d292e03
Arseniy Usik <senya769@mail.ru>
2022-05-28 19:07:19 +0300

    finished the tasks of concurrency

--- src/arseniyusik/lesson18_concurrency/task5/Application.java
+++ src/arseniyusik/lesson18_concurrency/task5/Application.java
@@ -2,26 +2,27 @@ package arseniyusik.lesson18_concurrency.task5;
 
 import arseniyusik.lesson18_concurrency.task5.model.SaveAsThread;
 
-import java.io.File;
+import java.io.*;
 import java.util.Random;
 
 public class Application {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         File file = new File("arrayThreads.txt");
+        FileWriter fileb = new FileWriter(file.getName());
         int[] arr1 = new int[5];
         int[] arr2 = new int[5];
         int[] arr3 = new int[5];
-        for (int i = 0; i < arr1.length; ++i){
-            arr1[i] = new Random().nextInt(30)+1;
-            arr2[i] = new Random().nextInt(30)+1;
-            arr3[i] = new Random().nextInt(30)+1;
+        for (int i = 0; i < arr1.length; ++i) {
+            arr1[i] = new Random().nextInt(30) + 1;
+            arr2[i] = new Random().nextInt(30) + 1;
+            arr3[i] = new Random().nextInt(30) + 1;
         }
         // SaveAsThread save1 = new SaveAsThread(arr1);
         // SaveAsThread save2 = new SaveAsThread(arr2);
         // SaveAsThread save3 = new SaveAsThread(arr3);
-        Thread thread1 = new Thread(new SaveAsThread(file,arr1));
-        Thread thread2 = new Thread(new SaveAsThread(file,arr2));
-        Thread thread3 = new Thread(new SaveAsThread(file,arr3));
+        Thread thread1 = new Thread(new SaveAsThread(arr1));
+        Thread thread2 = new Thread(new SaveAsThread(arr2));
+        Thread thread3 = new Thread(new SaveAsThread(arr3));
         thread1.start();
         thread2.start();
         thread3.start();
